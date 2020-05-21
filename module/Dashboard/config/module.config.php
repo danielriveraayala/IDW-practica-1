174f2b72f59323093efc04334d2a0db4990d5f4e
Daniel Esau Rivera Ayala <danielriveraayala@gmail.com>
2020-05-20 23:44:38 -0500

    Subida inicial

---
+++ module/Dashboard/config/module.config.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Dashboard;
+
+use Laminas\Router\Http\Literal;
+use Laminas\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'dashboard' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/dashboard',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'Dashboard' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Dashboard/src/Module.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Dashboard;
+
+use Laminas\ModuleManager\ModuleManager;
+use Laminas\Mvc\MvcEvent;
+
+class Module
+{
+    public function getConfig(): array
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function init(ModuleManager $manager)
+    {
+        // Get event manager.
+        $eventManager = $manager->getEventManager();
+        $sharedEventManager = $eventManager->getSharedManager();
+        // Register the event listener method.
+        $sharedEventManager->attach(__NAMESPACE__, 'dispatch',
+            [$this, 'onDispatch'], 100);
+    }
+
+    public function onDispatch(MvcEvent $event)
+    {
+        // Get controller to which the HTTP request was dispatched.
+        $controller = $event->getTarget();
+        // Get fully qualified class name of the controller.
+        $controllerClass = get_class($controller);
+        // Get module name of the controller.
+        $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+
+        // Switch layout only for controllers belonging to our module.
+        if ($moduleNamespace == __NAMESPACE__) {
+            $viewModel = $event->getViewModel();
+            $viewModel->setTemplate('partial/layout');
+        }
+    }
+}
